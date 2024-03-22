ab6465606f457f60746fa7ae8ab47e582bd70b9f
Pavel <verbipo@gmail.com>
2024-03-22 11:56:50 +0100

    feat: getCountDaysInMonth , getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -97,8 +97,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const lastDayOfMonth = new Date(year, month, 0);
+  return lastDayOfMonth.getDate();
 }
 
 /**
@@ -112,8 +113,9 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const differenceInMilliseconds = new Date(dateEnd) - new Date(dateStart);
+  return Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24)) + 1;
 }
 
 /**
